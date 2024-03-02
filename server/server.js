2a290e332a716c7a9898e0267a1c404acb15cd0b
Youtmax <youtmax654@gmail.com>
2024-03-02 17:22:57 +0100

    [REFACTOR] Client folder and Server folder, Node.js server created and get FR and EN random word API

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fs = require("fs");
+const PORT = 8080;
+
+const getRandomWord = (filename, callback) => {
+  fs.readFile("./words/" + filename, "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      const words = data.split("\r\n");
+      const randomIndex = Math.floor(Math.random() * words.length);
+      callback(words[randomIndex]);
+    }
+  });
+}
+
+app.use(cors());
+
+app.get("/api/fr/word", (req, res) => {
+  getRandomWord("fr.txt", (word) => {
+    res.json({ "word": word });
+  });
+});
+
+app.get("/api/en/word", (req, res) => {
+  getRandomWord("en.txt", (word) => {
+    res.json({ "word": word });
+  });
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});

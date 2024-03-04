51d1aacf1f2d47f6214605135e13ff6a22dda14d
Youtmax <youtmax654@gmail.com>
2024-03-04 21:48:22 +0100

    [FIX] There wasn't build command for node js server

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,12 +1,5 @@
-const path = require('path');
-
 module.exports = {
   mode: 'production',
   entry: './server.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    publicPath: '/',
-    filename: 'final.js',
-  },
   target: 'node',
 };
